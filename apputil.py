f38c1cf61ca8f24dd780f8f7bf5e99d5047be6d0
Divya Kapoor <38455002+divyakapoor23@users.noreply.github.com>
2025-09-05 17:12:13 +0000

    Initial Commit

--- apputil.py
+++ apputil.py
@@ -1,13 +1,27 @@
 import numpy as np
 
-
 # update/add code below ...
 
 def ways(n):
-    return None
+    if n < 0:
+        return 0;
+    return n // 5 + 1
 
 def lowest_score(names, scores):
-    return None
+    names = np.array(names)
+    scores = np.array(scores)
+    idx = np.argmin(scores)
+    return names[idx]
 
 def sort_names(names, scores):
-    return None
+    names = np.array(names)
+    scores = np.array(scores)
+    # if len(names) != len(scores):
+    idx_sorted = np.argsort(scores)[::-1]
+    return names[idx_sorted]
+    
+
+
+print(ways(5))
+print(lowest_score(['A', 'B', 'C'], [90, 80, 85]))
+print(sort_names(['A', 'B', 'C'], [90, 80, 85]))
